afe308bc02e8422e5932208b81bfea2b7e49fbcc
xiaotong <xd59@njit.edu>
2024-08-09 00:17:07 -0400

    use hash router
    github pages doesn't support browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './pages/errorPage';
 import Layout from './layouts/layout';
 import './services/firebase';
@@ -8,34 +8,31 @@ import MyExperimentsList from './pages/myExperimentsList';
 import ExperimentAnalyzer from './pages/experimentAnalyzer/experimentAnalyzer';
 
 const App = () => {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '',
-        element: <Layout />,
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            path: '',
-            element: <HomePage />,
-          },
-          {
-            path: 'myExperimentsList',
-            element: <MyExperimentsList />,
-          },
-          {
-            path: 'experiments/:expType/:userId/:expId',
-            element: <ExperimentAnalyzer />,
-          },
-          {
-            path: '*',
-            element: <ErrorPage />,
-          },
-        ],
-      },
-    ],
-    { basename: '/infrared-explorer-web' },
-  );
+  const router = createHashRouter([
+    {
+      path: '',
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '',
+          element: <HomePage />,
+        },
+        {
+          path: 'myExperimentsList',
+          element: <MyExperimentsList />,
+        },
+        {
+          path: 'experiments/:expType/:userId/:expId',
+          element: <ExperimentAnalyzer />,
+        },
+        {
+          path: '*',
+          element: <ErrorPage />,
+        },
+      ],
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 };
